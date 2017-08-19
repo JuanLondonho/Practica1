7ca0466fa299b9c39b612e55e4e234c08ee0e6f8
davidblj <david.blj95@gmail.com>
2017-08-19 17:14:18 -0500

    Log in and register.

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20172/lab1/MainActivity.java
@@ -0,0 +1,33 @@
+package co.edu.udea.compumovil.gr05_20172.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.btnLogin);
+        button.setOnClickListener(this);
+        //TextView textView = (TextView)findViewById(R.id.txtViewRegister);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                break;
+
+            case R.id.txtViewRegister:
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
+}
