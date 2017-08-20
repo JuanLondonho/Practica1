504bede1c791d4be25a1844db579d4432fde5ab3
JuanLondonho <Juan.londono11@udea.edu.co>
2017-08-20 13:53:04 -0500

    LandScape and Image from Gallery

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20172/lab1/RegisterActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20172/lab1/RegisterActivity.java
@@ -1,22 +1,37 @@
 package co.edu.udea.compumovil.gr05_20172.lab1;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 // 24 or higher
 //import android.icu.util.Calendar;
 import java.util.Calendar;
+
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import static android.R.attr.data;
+import static android.R.attr.layout_row;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener{
 
     //DatePickerDialog.OnDateSetListener  mDateSetListener;
     TextView displayDate;
+    private static final int SELECTED_PICTURE=1;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         displayDate = (TextView) findViewById(R.id.tvDate);
         displayDate.setOnClickListener(this);
+        iv = (ImageView)findViewById(R.id.imgUser);
+
     }
 
     @Override
@@ -46,7 +63,42 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
                 break;
+
+            case R.id.imgUser:
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, SELECTED_PICTURE);
+                break;
         }
+    }
+
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case SELECTED_PICTURE:
+                if(resultCode==RESULT_OK){
+                    Uri uri = data.getData();
+                    String[]projection = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(uri, projection,null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(projection[0]);
+                    String filePath = cursor.getString(columnIndex);
+
+                    cursor.close();
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+                    Drawable d = new BitmapDrawable(yourSelectedImage);
+
+                    System.out.println("STEP1");
+                    iv.setBackground(d);
+                    iv.setImageResource(0);
+
+                }
+        }
+
+
     }
 
     @Override
