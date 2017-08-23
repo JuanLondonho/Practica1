c85c5890ade0b8c66d8a6ea878e9c3bbedd75c35
davidblj <david.blj95@gmail.com>
2017-08-22 21:42:53 -0500

    Image storage

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20172/lab1/RegisterActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20172/lab1/RegisterActivity.java
@@ -1,16 +1,25 @@
 package co.edu.udea.compumovil.gr05_20172.lab1;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 // 24 or higher
 //import android.icu.util.Calendar;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 
 import android.graphics.drawable.Drawable;
@@ -19,6 +28,7 @@ import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +44,8 @@ import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+import static java.lang.System.out;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener, PlaceSelectionListener{
 
@@ -59,7 +71,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     // image setup
 
-    ImageView image;
+    ImageView imageFile;
+    String route;
 
 
     @Override
@@ -128,8 +141,14 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 break;
 
             case R.id.btnUserRegister:
-                Log.d("TAG", "case3");
 
+                // set the image as a Bitmap object to store it in the internal memory.
+                imageFile = (ImageView) findViewById(R.id.imgUser);
+                imageFile.setDrawingCacheEnabled(true);
+                imageFile.buildDrawingCache();
+                Bitmap bitmap =  Bitmap.createBitmap(imageFile.getDrawingCache());
+
+                route = saveToInternalStorage(bitmap);
 
                 name = ((EditText) findViewById(R.id.txtUserName)).getText().toString();
                 lastName = ((EditText) findViewById(R.id.txtUserLastName)).getText().toString();
@@ -145,6 +164,18 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 address = ((EditText) findViewById(R.id.txtUserAddress)).getText().toString();
                 email = ((EditText) findViewById(R.id.txtUserEmail)).getText().toString();
                 password =  ((EditText) findViewById(R.id.txtUserPassword)).getText().toString();
+
+                mEditor.putString("route", route);
+                mEditor.putString("name", name);
+                mEditor.putString("lastName", lastName);
+                mEditor.putString("gender", gender);
+                mEditor.putString("birthDate", birthDate);
+                mEditor.putString("phone", phone);
+                mEditor.putString("address", address);
+                mEditor.putString("email", email);
+                mEditor.putString("password", password);
+                mEditor.putString("city", city);
+                mEditor.commit();
 
                 if(name.equals("") || lastName.equals("") || gender.equals("") || phone.equals("")
                         || address.equals("") || email.equals("") || password.equals("") || city.equals("")
@@ -166,21 +197,48 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     mEditor.putString("city", city);
                     mEditor.commit();
 
+                    // check if variables were saved in shared preferences.
+                    checkSharedPreferences();
+
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
                 }
-
-                // check if variables were saved in shared preferences.
 
                 break;
         }
     }
 
+
     private void checkSharedPreferences() {
         String name = mPreferences.getString("name", "");
         String password = mPreferences.getString("password", "");
+        String route = mPreferences.getString("route","");
 
-        Log.d("TAG", "name: " + name + " password: " + password);
+        Log.d("TAG", "name: " + name + " password: " + password + " route:" + route);
+    }
+
+    private String saveToInternalStorage(Bitmap bitmapImage) {
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory, "profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 
     protected  void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -203,14 +261,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                     Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
                     Drawable d = new BitmapDrawable(yourSelectedImage);
 
-                    System.out.println("STEP1");
+                    out.println("STEP1");
                     iv.setBackground(d);
                     iv.setImageResource(0);
 
                 }
         }
-
-
     }
 
     @Override
